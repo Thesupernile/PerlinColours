475bc5a84dde1b80825d2a2af2f485954a06273e
Thesupernile <thesupernile6@gmail.com>
2025-08-11 01:00:29 +0100

    Added Perlin Noise calculation (untested

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-class PerlinNoiseGrid {
+class PerlinNoiseGrid2D {
 
-    constructor(gridSize, gridDensity, ){
+    constructor(gridSize){
         this.gridSize = gridSize;
-        this.gridDensity = gridDensity;
         this.grid = [[]];
         this.#generateGrid();
     }
@@ -21,7 +20,41 @@ class PerlinNoiseGrid {
     }
 
     calculatePerlin(x, y){
+        // Find the bottom left point in the square
+        let gridX = Math.floor(x);
+        let gridY = Math.floor(y);
 
+        let points = [[gridX, gridY], [gridX + 1, gridY], [gridX, gridY + 1], [gridX + 1, gridY + 1]];
+        let pointValues = [];
+
+        for (point[pointX, pointY] of points){
+            const angle = grid[pointY][pointX];
+            // Calculate the distances to the required points
+            const XDist = Math.abs((x - gridX) - (pointX - gridX));
+            const YDist = Math.abs((y - gridY) - (pointY - gridY));
+
+            // Calculate the dot product
+            let pointValue = XDist * Math.cos(angle) + YDist * Math.sin(angle);
+
+            pointValues.push(pointValue);
+        }
+
+        // Interpolation
+
+        // Find what percent along the X axis we are
+        let XPercent = (x - gridX) * 100;
+        let semiInterpolatedValues = [];
+        for (let i = 0; i < pointValues.length; i += 2){
+            let semiInterpolatedValue = (1 - XPercent) * pointValues[i] + XPercent * pointValues[i + 1];
+            semiInterpolatedValue.push(semiInterpolatedValue);
+        }
+
+        let YPercent = (y - gridY) * 100;
+        let finalValue = (1 - YPercent) * semiInterpolatedValues[0] + YPercent * semiInterpolatedValues[1];
+
+        return finalValue;
     }
 
 }
+
+let perlinNoise = new PerlinNoiseGrid2D(10);
